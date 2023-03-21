0fe8df3548da0feb1cf0721d449b1dcfd09fc9e7
Yuriy Perminov <yuriy.perminov@gmail.com>
2023-03-21 23:38:20 +0000

    add Modal

---
+++ src/frontend/src/common/components/PizzaModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="popup">
+    <a href="#" class="close" @click="close">
+      <span class="visually-hidden">Закрыть попап</span>
+    </a>
+    <div class="popup__title">
+      <h2 class="title">Спасибо за заказ</h2>
+    </div>
+    <p>Мы начали готовить Ваш заказ, скоро привезём его вам ;)</p>
+    <div class="popup__button">
+      <a href="#" class="button" @click="close">Отлично, я жду!</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
